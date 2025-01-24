4d42ffc1d4a7bc6efb8226044ce7ce0c29298d69
sbx_user1051 <sbx_user1051@169.254.6.45>
2025-01-24 16:42:11 +0000

    Fri Jan 24 16:42:11 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,21 @@
 
+PRICE_TABLE = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15
+}
 
+SPECIAL_OFFERS = {
+    "A": (3, 130),
+    "B": (2, 45)
+}
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> int:
+    # check if all skus are valid
+    for sku in skus:
+        if sku not in PRICE_TABLE:
+            return -1
+
+    
